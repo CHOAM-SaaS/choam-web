31837ea07900151c05f944a0b69e87503fb3cea4
dnorm <dnorman1611@gmail.com>
2024-07-18 00:38:03 -0500

    working on authentication, shoulda come first tbh

---
+++ src/components/DiscordLogin.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="https://discord.com/oauth2/authorize?client_id=1227073029759041597&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A57159%2Fauth%2Fdiscord&scope=identify"
+  class="flex items-center px-5 py-3 space-x-4 font-bold rounded-md text-x1">
+    <i class="fa-brands fa-discord text-2x1"></i>
+    <span>Login with Discord</span>
+  </a>
+</template>
+
+<script setup>
+
+
+</script>
